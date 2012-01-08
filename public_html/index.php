28d654300fb73fb319fc2b8799cfe3580a725e97
Michelangelo Giacomelli <michelangelog@gmail.com>
2012-01-08 17:50:47 +0100

    little fix

--- public_html/index.php
+++ public_html/index.php
@@ -2,8 +2,9 @@
 // Microframeworks: slim & underscore
 // note this is my generic app template
 // not sure using underscore in this project
-require_once '../vendor/Slim/Slim.php';
-require_once '../vendor/idiorm.php';
+require_once './vendor/Slim/Slim.php';
+require_once './vendor/underscore.php';
+require_once './vendor/idiorm.php';
 
 // i now need max, probably in future min and other
 class ORM_EXT extends ORM
@@ -15,30 +16,31 @@ class ORM_EXT extends ORM
     }
     public function max($column)
     {
+        if(method_exists('ORM', 'max')) return parent::max($column);
         $this->select_expr('MAX('.$column.')', 'maxvalue');
         $result = $this->find_one();
         return ($result !== false && isset($result->maxvalue)) ? (int) $result->maxvalue : 0;
     }
     public function min($column)
     {
+        if(method_exists('ORM', 'min')) return parent::min($column);
         $this->select_expr('MIN('.$column.')', 'minvalue');
         $result = $this->find_one();
         return ($result !== false && isset($result->minvalue)) ? (int) $result->minvalue : 0;
     }
 }
-
 // custom setting
 $app = new Slim(array(
     'log.enable' => true,
-    'log.path' => '../log',
+    'log.path' => './log',
     'log.level' => 4,
-    'templates.path' => '../app/views',
-    'connection.string' => 'sqlite:../data/url.sqlite',
+    'templates.path' => './app/views',
+    'connection.string' => 'sqlite:./data/url.sqlite',
     'service.name' => $_SERVER['SERVER_NAME'] . '/',
 ));
 
 // application routes
-require '../app/routes/urls.php';
+require './app/routes/urls.php';
 
 // run app!
 $app->run();
